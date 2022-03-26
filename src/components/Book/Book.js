446cd168b081d87834f8e78b062ee3264f4530c9
Tufahel <tufahel96@gmail.com>
2022-03-26 16:34:39 +0600

    remove button added

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -7,28 +7,29 @@ import './Book.css'
 function Book(props) {
   const { title, author, category, progress, chapter } = props;
   return (
-    <div className="grid-container">
-      <div className="grid-item items-1">
-        <p>{category}</p>
-        <h2>{title}</h2>
-        <p className="author">{author}</p>
-      </div>
-      <div className="grid-item items-2">
-        <div style={{ width: 100, height: 100}}>
-        <CircularProgressbar value={progress} />
+      <div className="grid-container">
+        <div className="grid-item items-1">
+          <p>{category}</p>
+          <h2>{title}</h2>
+          <p className="author">{author}</p>
         </div>
-        <div className="progress">
-        <p className="percent">{progress}%</p>
-        <p className="completed">Completed</p>
+        <div className="grid-item items-2">
+          <div style={{ width: 100, height: 100 }}>
+            <CircularProgressbar value={progress} />
+          </div>
+          <div className="progress">
+            <p className="percent">{progress}%</p>
+            <p className="completed">Completed</p>
+          </div>
         </div>
+        <div className="grid-item vl"></div>
+        <div className="grid-item items-3">
+          <p className="current">Current Progress</p>
+          <p>{chapter}</p>
+          <button type="button">Update Progress</button>
+        </div>
+        <button type="button" className="remove-btn">Remove</button>
       </div>
-      <div className="grid-item vl"></div>
-      <div className="grid-item items-3">
-        <p className="current">Current Progress</p>
-        <p>{chapter}</p>
-        <button type="button">Update Progress</button>
-      </div>
-    </div>
   );
 }
 
