b5c3c233fcc191d2ab11b70dd87e0b1d20aba2ee
Tufahel <tufahel96@gmail.com>
2022-04-02 17:33:13 +0600

    basic styling

--- src/components/AddBook/AddBook.js
+++ src/components/AddBook/AddBook.js
@@ -5,56 +5,54 @@ import { addBook } from '../../redux/Books/Books';
 
 function AddBook() {
   const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
+  const [author, setAuthor] = useState('Author Name');
   const [category, setCategory] = useState('');
   const dispatch = useDispatch();
   return (
-    <div>
-      <form action="">
+    <div className="container-fluid">
+      <form>
         <p className="form-header">Add New Book</p>
-        <div className="inputs">
-          <label htmlFor="title">
-            <input
-              type="text"
-              id="title"
-              name="title"
-              placeholder="Book Name...."
-              onChange={(e) => setTitle(e.target.value)}
-              required
-            />
-          </label>
-          <label htmlFor="author">
-            <input
-              type="text"
-              id="author"
-              name="author"
-              placeholder="Author Name...."
-              onChange={(e) => setAuthor(e.target.value)}
-              required
-            />
-          </label>
-          <label htmlFor="category">
-            <input
-              name="category"
-              type="text"
-              placeholder="Category"
-              list="category"
-              onChange={(e) => setCategory(e.target.value)}
-              required
-            />
-            <datalist id="category">
-              <option value="Action">Action</option>
-              <option value="Fiction">Fiction</option>
-              <option value="Non Fiction">Non Fiction</option>
-              <option value="Phylosophy">Phylosophy</option>
-            </datalist>
-          </label>
-          <button
-            type="submit"
-            onClick={(event) => { event.preventDefault(); dispatch(addBook(title, author, category), setAuthor(''), setTitle('')); }}
-          >
-            Add Book
-          </button>
+        <div className="row">
+          <div className="col-sm-12 col-md-12 col-lg-6">
+            <label htmlFor="title">
+              <input
+                type="text"
+                id="title"
+                name="title"
+                placeholder="Book Name...."
+                onChange={(e) => setTitle(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+          <div className="col-sm-12 col-md-12 col-lg-4">
+            <label htmlFor="category">
+              <input
+                id="category"
+                name="category"
+                type="text"
+                placeholder="Category"
+                list="category"
+                onChange={(e) => setCategory(e.target.value)}
+                required
+              />
+              <datalist id="category">
+                <option value="Action">Action</option>
+                <option value="Fiction">Fiction</option>
+                <option value="Non Fiction">Non Fiction</option>
+                <option value="Phylosophy">Phylosophy</option>
+              </datalist>
+            </label>
+          </div>
+          <div className="col-sm-12 col-md-12 col-lg-2">
+            <button
+              className="submit-btn"
+              type="submit"
+              onClick={(event) => { event.preventDefault(); dispatch(addBook(title, author, category), setAuthor('Author Name'), setTitle('')); }}
+            >
+              Add Book
+            </button>
+          </div>
         </div>
       </form>
     </div>
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { removeBook } from '../../redux/Books/Books';
+import { useSelector } from 'react-redux';
 import AddBook from '../AddBook/AddBook';
 import Book from '../Book/Book';
 import './Books.css';
 
 function Books() {
   const list = useSelector((state) => state.booksReducers.list);
-  const dispatch = useDispatch();
   const randomNumber = Math.floor((Math.random() * 100) + 1);
   const stringRandom = randomNumber.toString();
   return (
     <div>
       <div>
         <div className="card">
-          <Book title="Default Book" author="Default Author" category="Default Category" progress="64" chapter="Chapter 17" />
-          <button
-            type="button"
-            className="remove-btn"
-          >
-            Remove Book
-          </button>
+          <Book title="Default Book" author="Default Author" category="Default Category" progress="64" chapter="Chapter 17" id="null" />
         </div>
       </div>
       <div>
@@ -28,17 +20,7 @@ function Books() {
           list.map((elem) => (
             <div key={elem.item_id}>
               <div className="card">
-                <Book title={elem.title} author={elem.author} category={elem.category} progress={stringRandom} chapter="Chapter 17" />
-                <button
-                  key={elem.item_id}
-                  type="button"
-                  className="remove-btn"
-                  onClick={() => {
-                    dispatch(removeBook(elem.item_id));
-                  }}
-                >
-                  Remove Book
-                </button>
+                <Book title={elem.title} author={elem.author} category={elem.category} progress={stringRandom} chapter="Chapter 17" id={elem.item_id} />
               </div>
             </div>
           ))
